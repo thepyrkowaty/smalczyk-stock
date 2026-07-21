843ae52ffbabff7646e69e0c7037d04f23b7b42c
thepyrkowaty <thepyrkowaty@gmail.com>
2026-07-21 18:32:04 +0200

    path changes

--- utils/run_script.sh
+++ utils/run_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/debian/data/.venv/bin/activate
-python3 /home/debian/data/other_data.py daily
-python3 /home/debian/data/yahoo_data.py daily
+python3 /home/debian/smalczyk-stock/utils/other_data.py daily
+python3 /home/debian/smalczyk-stock/utils/yahoo_data.py daily
